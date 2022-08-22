81256e37c2321df44c37ff9cf422b5f4789d9a2e
henryzord <henry.cagnini@gmail.com>
2022-08-22 09:55:49 -0300

    resolvi a iteração dos nomes e preços dos produtos

--- pythoncrawler.py
+++ pythoncrawler.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 import os
 
+from selenium.webdriver.remote.webelement import WebElement
+
 
 def main():
     if os.name == 'nt':
@@ -14,12 +16,14 @@ def main():
         # abre uma instância do firefox na página dada
         driver.get('https://www.netshoes.com.br/busca?nsCat=Natural&q=handebol+asics&genero=feminino')  # link do produto
         # adicionando a tag onde esta o preço
-        nome_produto = driver.find_elements_by_tag_name("div.item-card__description__product-name")
-        preco_produto = driver.find_elements_by_tag_name("div.pr")
+        nome_produtos = driver.find_elements_by_tag_name("div.item-card__description__product-name")  # type: list
+        preco_produtos = driver.find_elements_by_tag_name("div.pr")  # type: list
         # imprimindo na tela o valor do produto
 
-        for produtos in nome_produto, preco_produto:
-            print(produtos.text)
+        for produto in zip(nome_produtos, preco_produtos):
+            nome = produto[0]  # type: WebElement
+            preco = produto[1]  # type: WebElement
+            print('Nome: %s Preço: %s' % (nome.text, preco.text))
         time.sleep(10)
 
 
