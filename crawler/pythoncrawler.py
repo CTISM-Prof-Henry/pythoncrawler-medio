2b7419cdbfd5223374307f01a7b06f780c0f582d
Bruno Barchet <brunombarchet@gmail.com>
2022-10-12 17:29:34 -0300

    relocando info

--- crawler/pythoncrawler.py
+++ crawler/pythoncrawler.py
@@ -21,9 +21,9 @@ def main():
             'https://www.netshoes.com.br/busca?nsCat=Natural&q=handebol+asics&genero=feminino')  # link do produto
 
         # adicionando a tag onde esta o preço
-        nome = driver.find_elements_by_tag_name("div.item-card__description__product-name")
-        preco = driver.find_elements_by_tag_name("span.haveInstallments")
-        desconto = driver.find_elements_by_tag_name("li.discount.text")
+        nome_produtos = driver.find_elements_by_tag_name("div.item-card__description__product-name")
+        preco_produtos = driver.find_elements_by_tag_name("span.haveInstallments")
+        desconto_produto = driver.find_elements_by_tag_name("li.discount.text")
         time.sleep(4)
         # abrindo conexao com banco
         database_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'banco.db')
@@ -42,22 +42,23 @@ def main():
             except:
                 ultimo_anota_id = 0
 
+            # zipando os produtos para manipulação
+            for produto in zip(nome_produtos, preco_produtos, desconto_produto):
+
+                nome = produto[0]
+                preco = produto[1]
+                desconto = produto[2]
                 # se tiver no banco
                 res = cur.execute('SELECT id_produto FROM PRODUTO WHERE nome=\'{0}\''.format(nome)).fetchone()
                 if res is None:  # se nao tiver nada no banco:
                     # insere id, nome, preco e desconto
                     cur.execute(
 <<<<<<< HEAD
-                        'INSERT OR REPLACE INTO PRODUTO(id_produto, nome, desconto) VALUES ({0}, \'{1}\', \'{2}\')'.format
-                        (ultimo_produto_id, nome.text, preco.text, desconto.text)
-=======
-<<<<<<< HEAD
                         'INSERT INTO PRODUTO(id_produto, nome, preco, desconto) VALUES ({0}, \'{1}\', \'{2}\', \'{3}\')'.format
 =======
                         'INSERT INTO PRODUTO(id_produto, nome, desconto) VALUES ({0}, \'{1}\', \'{2}\')'.format
 >>>>>>> 7dcada7f1c96d073263a9496caf224ec76c353bf
                         (ultimo_produto_id, nome, preco, desconto)
->>>>>>> d529ca77831828ce33a8ea3c1acbec1d027aa681
                     )
                     # definindo id_produto
                     id_produto = ultimo_produto_id
@@ -67,17 +68,18 @@ def main():
 
                 # inserindo na tabela ANOTA
                 cur.execute(
-                    'INSERT OR REPLACE INTO ANOTA(id_anota, data_crawler, preco) VALUES ({0}, \'{1}\', \'{2}\')'.format(
-                        ultimo_anota_id, data_crawler, preco.text)
+                    'INSERT INTO ANOTA(id_anota, data_crawler, preco) VALUES ({0}, \'{1}\', \'{2}\')'.format(
+                        ultimo_anota_id, data_crawler, 'R$ 0,00')
                 )
 
                 # inserindo na tabela de intersecção PRODUTO_e_ANOTA
                 cur.execute(
-                    'INSERT OR REPLACE INTO PRODUTO_e_ANOTA(id_produto, id_anota) VALUES ({0}, {1})'.format(
+                    'INSERT INTO PRODUTO_e_ANOTA(id_produto, id_anota) VALUES ({0}, {1})'.format(
                         id_produto, ultimo_anota_id
                     )
                 )
                 ultimo_anota_id += 1
+
                 con.commit()
                 # fechando conexão com o banco
                 con.close()
@@ -85,3 +87,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
