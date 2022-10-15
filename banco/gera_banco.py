89c7e0a9389cc09ef78b5bee7adfb16094581345
luize Baldoni <oliveira.luize@acad.ufsm.br>
2022-10-15 17:17:16 -0300

    ajuestes no banco de dados e no crawler

--- banco/gera_banco.py
+++ banco/gera_banco.py
@@ -1,41 +1,53 @@
 # código muito simplificado para criar um banco de dados de exemplo.
 import sqlite3
+import os
 
-# abre uma conexão
-con = sqlite3.connect('banco.db')
 
-# pega um cursor, que é o objeto que irá executar as transações
-cur = con.cursor()
+def main():
+    # abre uma conexão
+    database_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../banco/banco.db')
 
-# cria tabela
-cur.execute('''
-            CREATE TABLE IF NOT EXISTS PRODUTO (
-                id_produto integer,
-                nome text, 
-                primary key (id_produto)
+    if os.path.exists(database_path):
+        os.remove(database_path)
+
+    # abre uma conexão com o banco e fecha automaticamente no fim do bloco with
+    with sqlite3.connect(database_path) as con:
+        # pega um cursor, que é o objeto que irá executar as transações
+        # pega um cursor, que é o objeto que irá executar as transações
+        cur = con.cursor()
+
+        # cria tabela
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS produto (
+                        id_produto integer,
+                        nome text, 
+                        primary key (id_produto)
+                    )
+        ''')
+
+        cur.execute('''
+                   CREATE TABLE IF NOT EXISTS anota (
+                       id_anota integer, 
+                       dia_crawler text, 
+                       preco text,
+                       primary key(id_anota)
+                   )
+               ''')
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS produto_e_anota (
+                id_anota integer not null, id_produto integer not null,
+                primary key(id_anota, id_produto),
+                foreign key (id_produto) references PRODUTO(id_produto)
+                foreign key (id_anota) references ANOTA(id_anota)
+        
             )
-''')
+        ''')
 
-cur.execute('''
-           CREATE TABLE IF NOT EXISTS ANOTA (
-               id_anota integer, 
-               dia_crawler text, 
-               preco text,
-               primary key(id_anota)
-           )
-       ''')
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS PRODUTO_e_ANOTA (
-        id_anota integer not null, id_produto integer not null,
-        primary key(id_anota, id_produto),
-        foreign key (id_produto) references PRODUTO(id_produto)
-        foreign key (id_anota) references ANOTA(id_anota)
+        # salva modificações
+        con.commit()
 
-    )
-''')
+        # fecha conexão com o banco
 
-# salva modificações
-con.commit()
 
-# fecha conexão com o banco
-con.close()
+if __name__ == '__main__':
+    main()
--- crawler/pythoncrawler.py
+++ crawler/pythoncrawler.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 import os
-from datetime import datetime as dt
-
+import time
 import sqlite3
-
-from selenium.webdriver.remote.webelement import WebElement
+from datetime import datetime as dt
 
 
 def main():
@@ -28,53 +24,55 @@ def main():
 
         for name in nome_produtos:
             nome = name.text
+            print(nome)
         for price in preco_produtos:
             preco = price.text
+            print(preco)
+        # FUNCIONANDO ATE AQUI!!!
 
         database_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../banco/banco.db')
         with sqlite3.connect(database_path) as con:
             cur = sqlite3.connect('../banco/banco.db')
             # pegando data do dia q rodar
             data_crawler = dt.now().strftime('%Y-%m-%d-%H-%M-%S')
+            print(data_crawler)
             # gerando os id da tabela produtos
             try:
-                ultimo_produto_id = int(cur.execute('SELECT MAX(id_produto) from produtos').fetchone()[0]) + 1
+                ultimo_produto_id = int(cur.execute('SELECT MAX(id_produto) from produto').fetchone()[0]) + 1
+                print(ultimo_produto_id)
             except:
                 ultimo_produto_id = 0
+                print(ultimo_produto_id)
             # gerando os id da tabela anota
             try:
-                ultimo_anota_id = int(cur.execute('SELECT MAX(id_anotacao) from anotacoes').fetchone()[0]) + 1
+                ultimo_anota_id = int(cur.execute('SELECT MAX(id_anota) from anota').fetchone()[0]) + 1
+                print(ultimo_anota_id)
             except:
                 ultimo_anota_id = 0
+                print(ultimo_anota_id)
 
                 # se tiver no banco
-                res = cur.execute('SELECT id_produto FROM PRODUTO WHERE nome=\'{0}\''.format(nome)).fetchone()
+                res = cur.execute('SELECT id_produto FROM produto WHERE nome=\'{0}\''.format(nome)).fetchone()
                 if res is None:  # se nao tiver nada no banco:
                     # insere id, nome, preco e desconto
                     cur.execute(
-                        'INSERT OR REPLACE INTO PRODUTO(id_produto, nome) VALUES ({0}, \'{1}\')'.format
-                        (ultimo_produto_id, nome)
-                    )
+                        'INSERT INTO produto(id_produto, nome) VALUES ({0}, \'{1}\')'.format(ultimo_produto_id, nome))
+                    con.commit()
                     # definindo id_produto
                     id_produto = ultimo_produto_id
                     ultimo_produto_id += 1
                 else:
                     id_produto = res[0]   # atribui o id recuperado a variável id_produto
-
                 # inserindo na tabela ANOTA
                 cur.execute(
-                    'INSERT OR REPLACE INTO ANOTA(id_anota, dia_crawler, preco) VALUES ({0}, \'{1}\', \'{2}\')'.format(
-                        ultimo_anota_id, data_crawler, preco)
-                )
+                    'INSERT INTO anota(id_anota, dia_crawler, preco) VALUES ({0}, \'{1}\', \'{2}\')'.format(ultimo_anota_id, data_crawler, preco))
+                con.commit()
 
                 # inserindo na tabela de intersecção PRODUTO_e_ANOTA
                 cur.execute(
-                    'INSERT OR REPLACE INTO PRODUTO_e_ANOTA(id_produto, id_anota) VALUES ({0}, {1})'.format(
-                        id_produto, ultimo_anota_id
-                    )
-                )
+                    'INSERT INTO produto_e_anota(id_produto, id_anota) VALUES ({0}, {1})'.format(id_produto, ultimo_anota_id))
+                con.commit()
                 ultimo_anota_id += 1
-
                 con.commit()
 
 
